9eb45a79a45edb8e9133d13845869d098085aeed
Nandkishor1771503 <160572418+Nandkishor1771503@users.noreply.github.com>
2025-09-03 23:07:48 +0530

    images/index.js bug

--- src/images/index.js
+++ src/images/index.js
@@ -1,10 +1,10 @@
 import html from "../images/Html.jpg";
-import css from "../images/css.jpg";
+import css from "../images/CSS.jpg";
 import Js from "../images/JS.png";
 import react from "../images/ReactJS.png";
 import Gsap from "../images/GSAP.jpeg";
 import motion from "../images/FRAMER.webp";
-import lucid from "../images/lucid.png";
+import lucid from "../images/LUCID.png";
 import git from "../images/GIT.jpg";
 import netlify from "../images/NETLIFY.jpg";
 
